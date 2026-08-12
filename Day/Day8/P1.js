e8079f5e8e30fc5395b6e7eadd967812b3f28955
yuvikabm <yuvikabm10@gmail.com>
2026-08-12 10:55:52 +0530

    JavaScript(JS) usage

---
+++ Day/Day8/P1.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const myButton = document.getElementById("external-js");
+
+  myButton.addEventListener("click", function () {
+    alert("Hello Learners, Good morning. This is External JS");
+  });
+});
