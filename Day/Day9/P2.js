d9a5c69dc7f171c1f08852ff10d4643070f0f9d4
yuvikabm <yuvikabm10@gmail.com>
2026-08-13 12:58:36 +0530

    slice() and spread (...) | Looping over Arrays | Searching in arrays

--- Day/Day9/P2.js
+++ Day/Day9/P2.js
@@ -54,3 +54,58 @@ colors.splice(2,1);
 console.log(colors);
 colors.splice(1,0,"orange","purple");
 console.log(colors);
+
+
+console.log("slice() and spread (...)");
+const nums1=[10,20,30,40,50];
+console.log(nums1);
+const part=nums1.slice(1,4);
+console.log(part);
+const copy=nums1.slice(); //full shallow copy
+console.log(copy);
+
+console.log("Spread Operator");
+const a = [1,2,30,45];
+console.log(a);
+const b = [...a,3,4]; // shallow copy
+console.log(b);
+const copy2=[...a];
+console.log(copy2);
+
+console.log("Looping over Arrays");
+const arr5 = [111,222,333,444,555,666];
+// for loop
+for (let i = 0; i < arr5.length; i++) {
+    console.log(i,arr5[i]);    
+}
+
+// for ... of
+console.log("for ... of");
+for (const value of arr5){
+    console.log(value);    
+}
+
+console.log("for Each");
+arr5.forEach((value, index)=>{
+    console.log(index,value);
+})
+
+// Searching in arrays
+console.log("Searching in arrays");
+const myArray = [99,88,77,66,55,66];
+console.log(myArray.indexOf(66));
+console.log(myArray.lastIndexOf(66));
+console.log(myArray.includes(555));
+
+
+// find()
+// findIndex()
+const users = [
+    {id:1,name:"Rakesh"},
+    {id:2,name:"Bipin"}
+];
+
+const user = users.find(student=>student.id === 2);
+console.log(user);
+const index = users.findIndex(student=>student.name === "Bipin");
+console.log(index);
